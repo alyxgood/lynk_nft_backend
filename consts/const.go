9c3a609f133db83a542cc338678b4ecb07cbf192
alyxgood <alyx2821@gmail.com>
2022-11-04 11:23:51 +0800

    feat: token image

--- consts/const.go
+++ consts/const.go
@@ -7,5 +7,5 @@ import (
 const (
 	JsonRpc        = "https://rpc.alyxchain.com"
 	LYNKNFTAddress = "0x779927B8c02d5cB6C90C1EBD2F714EF4092C8b0e"
-	TokenImage     = "https://img.xxxx.com/nft/%d.png"
+	TokenImage     = "https://test.lynk.im/nft/%d.png"
 )
